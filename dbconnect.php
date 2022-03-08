4763370ac13810079f45e1bf50cb0cccf3288d18
edy susilo <81127445+olisuside@users.noreply.github.com>
2022-03-08 19:32:05 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","id18195070_root","Olisuside69*","id18195070_blongstore");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
---
+++ id18195070_blongstore.sql
@@ -0,0 +1,318 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 04, 2022 at 01:08 PM
+-- Server version: 10.5.12-MariaDB
+-- PHP Version: 7.3.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id18195070_blongstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `idcart` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `tglorder` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(10) NOT NULL DEFAULT 'Cart'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`idcart`, `orderid`, `userid`, `tglorder`, `status`) VALUES
+(10, '15wKVT0nej25Y', 2, '2020-03-16 12:10:42', 'Selesai'),
+(12, '15PzF03ejd8W2', 1, '2020-05-13 02:40:48', 'Confirmed'),
+(13, '16we5aMtCZVuA', 2, '2021-12-29 17:49:45', 'Confirmed'),
+(14, '16kbxdxBXtsBk', 3, '2021-12-30 02:21:15', 'Payment');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailorder`
+--
+
+CREATE TABLE `detailorder` (
+  `detailid` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `idproduk` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detailorder`
+--
+
+INSERT INTO `detailorder` (`detailid`, `orderid`, `idproduk`, `qty`) VALUES
+(16, '16we5aMtCZVuA', 11, 1),
+(17, '16kbxdxBXtsBk', 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int(11) NOT NULL,
+  `namakategori` varchar(20) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `namakategori`, `tgldibuat`) VALUES
+(4, 'Kaos Polos', '2021-12-29 17:18:08'),
+(5, 'Kaos Stripe', '2021-12-29 17:19:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfirmasi`
+--
+
+CREATE TABLE `konfirmasi` (
+  `idkonfirmasi` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `payment` varchar(10) NOT NULL,
+  `namarekening` varchar(25) NOT NULL,
+  `tglbayar` date NOT NULL,
+  `tglsubmit` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `konfirmasi`
+--
+
+INSERT INTO `konfirmasi` (`idkonfirmasi`, `orderid`, `userid`, `payment`, `namarekening`, `tglbayar`, `tglsubmit`) VALUES
+(1, '15PzF03ejd8W2', 1, 'Bank BCA', 'Admin', '2020-05-16', '2020-05-13 02:41:51'),
+(2, '16we5aMtCZVuA', 2, 'DANA', 'Guest', '2021-12-31', '2021-12-29 17:55:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `userid` int(11) NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `tgljoin` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(7) NOT NULL DEFAULT 'Member',
+  `lastlogin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`userid`, `namalengkap`, `email`, `password`, `notelp`, `alamat`, `tgljoin`, `role`, `lastlogin`) VALUES
+(1, 'Admin', 'admin', '$2y$10$GJVGd4ji3QE8ikTBzNyA0uLQhiGd6MirZeSJV1O6nUpjSVp1eaKzS', '01234567890', 'Indonesia', '2020-03-16 11:31:17', 'Admin', NULL),
+(2, 'Guest', 'guest', '$2y$10$xXEMgj5pMT9EE0QAx3QW8uEn155Je.FHH5SuIATxVheOt0Z4rhK6K', '01234567890', 'Indonesia', '2020-03-16 11:30:40', 'Member', NULL),
+(3, 'Budi', 'budi@mail.com', '$2y$10$E.NIbjSa1FEjmyhxB2GhIuQdYxYERXOA/B9DI/e4Lmp/qCU9wl7wK', '0813456789', 'Jakarta', '2021-12-29 18:06:20', 'Member', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `no` int(11) NOT NULL,
+  `metode` varchar(25) NOT NULL,
+  `norek` varchar(25) NOT NULL,
+  `logo` text DEFAULT NULL,
+  `an` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`no`, `metode`, `norek`, `logo`, `an`) VALUES
+(1, 'Bank BCA', '13131231231', 'images/bca.jpg', 'Tokopekita'),
+(2, 'Bank Mandiri', '943248844843', 'images/mandiri.jpg', 'Tokopekita'),
+(3, 'DANA', '0882313132123', 'images/dana.png', 'Tokopekita');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `idproduk` int(11) NOT NULL,
+  `idkategori` int(11) NOT NULL,
+  `namaproduk` varchar(30) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `hargabefore` int(11) NOT NULL,
+  `hargaafter` int(11) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `idkategori`, `namaproduk`, `gambar`, `deskripsi`, `rate`, `hargabefore`, `hargaafter`, `tgldibuat`) VALUES
+(8, 4, 'Kaos Polos Pink', 'produk/16ydvNmPC56LM.jpg', 'Bahan adem dan tidak panas', 4, 50000, 45000, '2021-12-29 17:29:10'),
+(11, 4, 'Kaos Polos Hitam', 'produk/.jpg', 'Bahan adem dan tidak panas', 4, 50000, 45000, '2021-12-29 17:37:46'),
+(12, 4, 'Kaos Polos Hijau', 'produk/Kaos Polos Hijau-.jpg', 'Bahan adem dan tidak panas', 4, 50000, 45000, '2021-12-29 17:44:33'),
+(13, 4, 'Kaos Polos Grey', 'produk/Kaos Polos Grey-.jpg', 'Bahan adem dan tidak panas', 5, 50000, 45000, '2021-12-29 17:45:25'),
+(14, 5, 'Kaos Stripe A', 'produk/Kaos Stripe A-.jpg', 'Bahan adem dan tidak panas', 5, 65000, 50000, '2021-12-29 17:47:15'),
+(15, 5, 'Kaos Stripe B', 'produk/Kaos Stripe B-.jpg', 'Bahan adem dan tidak panas', 5, 65000, 50000, '2021-12-29 17:48:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`idcart`),
+  ADD UNIQUE KEY `orderid` (`orderid`),
+  ADD KEY `orderid_2` (`orderid`);
+
+--
+-- Indexes for table `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD PRIMARY KEY (`detailid`),
+  ADD KEY `orderid` (`orderid`),
+  ADD KEY `idproduk` (`idproduk`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indexes for table `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD PRIMARY KEY (`idkonfirmasi`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idproduk`),
+  ADD KEY `idkategori` (`idkategori`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `detailorder`
+--
+ALTER TABLE `detailorder`
+  MODIFY `detailid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  MODIFY `idkonfirmasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD CONSTRAINT `idproduk` FOREIGN KEY (`idproduk`) REFERENCES `produk` (`idproduk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `cart` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `login` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `idkategori` FOREIGN KEY (`idkategori`) REFERENCES `kategori` (`idkategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
